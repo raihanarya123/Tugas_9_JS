327f65ab18fb73ad43bf420d6c1da2a74b95fca9
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:10:36 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs9.js
@@ -0,0 +1,7 @@
+function tugas9() {
+    var mahasiswa = {nama:"Raihan", nim:"2440040822", jurusan:"Teknik Informatika"};
+    for (var x in mahasiswa) {
+        console.log(mahasiswa[x]);
+    }
+}
+tugas9();
